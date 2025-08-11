42bab3915736bb9b501f24e9e5914be3d8b4bb67
JSalvo54 <112729473+SalvJosh@users.noreply.github.com>
2025-08-11 14:19:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
-const catRoutes = require("./routes/cats");
+const catRoutes = require("./cats");
 require("dotenv").config();
 
 const app = express();
